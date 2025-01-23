5a4e8f29b9acf4b7a3ba533f7d3f632fb34c8ab1
cbxaf <381356914@qq.com>
2025-01-24 00:29:11 +0800

    添加注解

--- js/glsl/clear.glsl
+++ js/glsl/clear.glsl
@@ -3,10 +3,10 @@
 precision highp float;
 precision mediump sampler2D;
 
-vUv: 这是传入的纹理坐标，通常由顶点着色器传递。
-uTexture: 输入的纹理，使用 sampler2D 类型，可能是场景中的颜色纹理。
-uWind: 输入的风场或其他场景数据的纹理，存储着压力值。
-value: 一个浮动常数，可能用来调整最终结果的强度。
+//vUv: 这是传入的纹理坐标，通常由顶点着色器传递。
+//uTexture: 输入的纹理，使用 sampler2D 类型，可能是场景中的颜色纹理。
+//uWind: 输入的风场或其他场景数据的纹理，存储着压力值。
+//value: 一个浮动常数，可能用来调整最终结果的强度。
 
 varying vec2 vUv;
 uniform sampler2D uTexture;
